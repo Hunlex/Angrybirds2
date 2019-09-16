f28aa2821215f5d7c78723664482e1121f109390
Охотников Алексей Дмитриевич <aleck.okhotnickov@yandex.ru>
2019-09-16 12:03:00 +0500

    init

---
+++ AngryBirdsTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		public static double FindSightAngle(double v, double distance)
+		{
+            var g = 9.8;
+            var s =v*v; 
+            var corner = 0.5*Math.Asin(distance*g/s);
+            return corner;
+		}
+	}
+}	
